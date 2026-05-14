ac5ecc7c8b3c140a13bde7bba0d5be8f88184245
xavierdon395-code <xavierdon395@gmail.com>
2026-05-14 13:57:59 +0800

    feat(ui): replace splash screen with Lumi deep-space theme
    - Generate 2732x2732 splash: gradient deep-space background, Big Dipper constellation with glowing Polaris, Lumi wordmark + BUILD YOUR CITY OF LEARNING tagline
    - Install @capacitor/splash-screen plugin for programmatic control
    - Configure SplashScreen in capacitor.config.json: 2s duration, autohide, #0f0e0c bg color, no spinner, fullscreen immersive
    - Replaces previous Capacitor default placeholder (white + X logo)

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.3.3"),
         .package(name: "CapacitorFilesystem", path: "../../../node_modules/@capacitor/filesystem"),
-        .package(name: "CapacitorShare", path: "../../../node_modules/@capacitor/share")
+        .package(name: "CapacitorShare", path: "../../../node_modules/@capacitor/share"),
+        .package(name: "CapacitorSplashScreen", path: "../../../node_modules/@capacitor/splash-screen")
     ],
     targets: [
         .target(
@@ -22,7 +23,8 @@ let package = Package(
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
                 .product(name: "CapacitorFilesystem", package: "CapacitorFilesystem"),
-                .product(name: "CapacitorShare", package: "CapacitorShare")
+                .product(name: "CapacitorShare", package: "CapacitorShare"),
+                .product(name: "CapacitorSplashScreen", package: "CapacitorSplashScreen")
             ]
         )
     ]
